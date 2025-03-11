88192cc91bcd8f705e0a4ef17975769ef74d5768
Reixx7 <reixx23and7@gmail.com>
2025-03-11 10:34:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let name = prompt("Ismingizni kiriting");
+let firstname = prompt("Familiyangizni kiriting");
+let vozrast = prompt("Yoshingizni kiriting");
+let lesson = prompt("Yoqtirgan faningizni yozing");
+let adres = prompt("Yashaydigan tumaningizni kiriting");
+alert(`Malumotlaringizni kiritganingiz  uchun tashakur`)
+
+console.log(`Ism:${name} `);
+console.log(`Familiya:${firstname} `);
+console.log(`Yosh:${vozrast} `);
+console.log(` Yoqtirgan fan:${lesson} `);
+console.log(` Yashash manzil:${adres} `);
+
